d948b070085fa2adf34c5075125fc83f30c8ccdc
vadimdk <vadim.dk81@gmail.com>
2025-03-05 16:37:25 +0200

    add initial application structure with routing and dashboard page

---
+++ src/pages/dashboard.tsx
@@ -0,0 +1,11 @@
+import React, { FC } from 'react'
+
+interface dashboardProps {}
+
+export const Dashboard: FC<dashboardProps> = ({  }) => {
+  return (
+    <div>
+     dashboard
+    </div>
+  )
+}
